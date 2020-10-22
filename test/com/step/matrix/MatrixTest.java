983b0ded68821666d6d9188f580db70ea72cd845
photongupta <rashmi224141@gmail.com>
2020-10-22 18:24:42 +0530

    modified test methods

--- test/com/step/matrix/MatrixTest.java
+++ test/com/step/matrix/MatrixTest.java
@@ -11,7 +11,7 @@ public class MatrixTest {
     int[][] sample1 = { { 1, 2 }, { 4, 5 }, { 7, 8 } };
     Matrix matrix1 = Matrix.create(sample1);
 
-    assertTrue("Should give instance of matrix", matrix1 instanceof Matrix);
+    assertTrue(matrix1 instanceof Matrix);
   }
 
   @Test
@@ -22,10 +22,7 @@ public class MatrixTest {
     int[][] sample2 = { { 1, 2 }, { 4, 5 }, { 7, 8 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    assertTrue(
-      "Should equate two matrices having same elements",
-      matrix1.equals(matrix2)
-    );
+    assertTrue(matrix1.equals(matrix2));
   }
 
   @Test
@@ -36,10 +33,7 @@ public class MatrixTest {
     int[][] sample2 = { { 4, 2 }, { 9, 5 }, { 7, 8 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    assertFalse(
-      "Should not equate two matrices having different elements",
-      matrix1.equals(matrix2)
-    );
+    assertFalse(matrix1.equals(matrix2));
   }
 
   @Test
@@ -50,10 +44,7 @@ public class MatrixTest {
     int[][] sample2 = { { 4 }, { 9 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    assertFalse(
-      "Should not equate two matrices having different dimensions",
-      matrix1.equals(matrix2)
-    );
+    assertFalse(matrix1.equals(matrix2));
   }
 
   @Test
@@ -61,12 +52,7 @@ public class MatrixTest {
     int[][] sample1 = { { 1, 2 }, { 4, 5 }, { 7, 8 } };
     Matrix matrix1 = Matrix.create(sample1);
 
-    Object obj = new Object();
-
-    assertFalse(
-      "Should not equate matrix with other object",
-      matrix1.equals(obj)
-    );
+    assertFalse(matrix1.equals(new Object()));
   }
 
   @Test
@@ -74,13 +60,7 @@ public class MatrixTest {
     int[][] sample1 = { { 1, 2 }, { 4, 5 } };
     Matrix matrix1 = Matrix.create(sample1);
 
-    String actual = matrix1.toString();
-    String expected = "1 2 \n4 5 \n";
-    assertEquals(
-      "Should give textual representation of matrix",
-      actual,
-      expected
-    );
+    assertEquals(matrix1.toString(), "1 2 \n4 5 \n");
   }
 
   @Test
@@ -91,12 +71,10 @@ public class MatrixTest {
     int[][] sample2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    Matrix sum = matrix1.add(matrix2);
-
     int[][] expectedData = { { 2, 4, 6 }, { 8, 10, 12 }, { 14, 16, 18 } };
     Matrix expected = Matrix.create(expectedData);
 
-    assertEquals("Should add two matrix of same dimension", sum, expected);
+    assertEquals(matrix1.add(matrix2), expected);
   }
 
   @Test
@@ -107,8 +85,7 @@ public class MatrixTest {
     int[][] sample2 = { { 1, 2 }, { 4, 5 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    Matrix sum = matrix1.add(matrix2);
-    assertNull("Should not add if two matrix have different dimension", sum);
+    assertNull(matrix1.add(matrix2));
   }
 
   @Test
@@ -119,16 +96,10 @@ public class MatrixTest {
     int[][] sample2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    Matrix difference = matrix1.subtract(matrix2);
-
     int[][] expectedData = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
     Matrix expected = Matrix.create(expectedData);
 
-    assertEquals(
-      "Should subtract second matrix from the first matrix if both have same dimension",
-      difference,
-      expected
-    );
+    assertEquals(matrix1.subtract(matrix2), expected);
   }
 
   @Test
@@ -139,11 +110,7 @@ public class MatrixTest {
     int[][] sample2 = { { 1, 2 }, { 4, 5 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    Matrix difference = matrix1.subtract(matrix2);
-    assertNull(
-      "Should not perform subtraction if two matrix have different dimension",
-      difference
-    );
+    assertNull(matrix1.subtract(matrix2));
   }
 
   @Test
@@ -154,12 +121,10 @@ public class MatrixTest {
     int[][] sample2 = { { 1, 2 }, { 4, 5 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    Matrix product = matrix1.multiply(matrix2);
-
     int[][] expectedData = { { 9, 12 }, { 24, 33 } };
     Matrix expected = Matrix.create(expectedData);
 
-    assertEquals("Should multiply two matrices", product, expected);
+    assertEquals(matrix1.multiply(matrix2), expected);
   }
 
   @Test
@@ -170,22 +135,14 @@ public class MatrixTest {
     int[][] sample2 = { { 1, 2 } };
     Matrix matrix2 = Matrix.create(sample2);
 
-    Matrix product = matrix1.multiply(matrix2);
-    assertNull(
-      "Should not multiply, if numbers of columns of first is not equal to the number of rows of the second matrix",
-      product
-    );
+    assertNull(matrix1.multiply(matrix2));
   }
 
   @Test
   public void shouldCalculateDeterminantOf2x2Matrix() {
     int[][] sample = { { 1, 2 }, { 4, 5 } };
     Matrix matrix = Matrix.create(sample);
-    assertEquals(
-      "Should give the determinant of given 2X2  matrix",
-      -3,
-      matrix.determinant()
-    );
+    assertEquals(-3, matrix.determinant());
   }
 
   @Test
@@ -197,10 +154,6 @@ public class MatrixTest {
       { 0, 3, 1, 1 },
     };
     Matrix matrix = Matrix.create(sample);
-    assertEquals(
-      "Should give the determinant of given nXn matrix",
-      -240,
-      matrix.determinant()
-    );
+    assertEquals(-240, matrix.determinant());
   }
 }
