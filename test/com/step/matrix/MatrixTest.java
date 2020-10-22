96703f05fe3aa57a556f022db91fc3e4147ba789
photongupta <rashmi224141@gmail.com>
2020-10-22 12:34:31 +0530

    added test for toString method of Matrix class

--- test/com/step/matrix/MatrixTest.java
+++ test/com/step/matrix/MatrixTest.java
@@ -33,4 +33,20 @@ public class MatrixTest {
       matrix1.equals(matrix2)
     );
   }
+
+  @Test
+  public void shouldGiveTextualRepresentation() {
+    System.out.println("\nTesting toString");
+
+    int[][] sample1 = { { 1, 2 }, { 4, 5 } };
+    Matrix matrix1 = Matrix.create(sample1);
+
+    String actual = matrix1.toString();
+    String expected = "1 2 \n4 5 \n";
+    assertEquals(
+      "Should give textual representation of matrix",
+      actual,
+      expected
+    );
+  }
 }
